f67d11a5ffab42e6040f733c6310d33691b863b0
shibainuudon <shibainuudon@gmail.com>
2022-04-29 23:20:54 +0900

    PrimitiveTopologyのサンプル追加 refs #41

---
+++ libs/render/samples/primitive_topology/sample_render_primitive_topology_main.cpp
@@ -0,0 +1,232 @@
+﻿/**
+ *	@file	sample_render_primitive_topology_main.cpp
+ *
+ *	@brief	PrimitiveTopologyを設定するサンプル
+ */
+
+#include <hamon/window.hpp>
+#include <hamon/render.hpp>
+#include <memory>
+#include <vector>
+
+namespace
+{
+
+hamon::Program GetGLSLProgram(void)
+{
+	return
+	{
+		hamon::ShaderLanguage::GLSL,
+		{
+			{
+				hamon::ShaderStage::Vertex,
+				R"(
+					#version 450
+					layout(location = 0) in vec3 aPosition;
+					layout(location = 1) in vec4 aColor;
+
+					layout(location = 0) out vec4 vColor;
+
+					void main()
+					{
+						gl_Position = vec4(aPosition, 1.0);
+						vColor = aColor;
+					}
+				)"
+			},
+			{
+				hamon::ShaderStage::Fragment,
+				R"(
+					#version 450
+					layout(location = 0) in vec4 vColor;
+
+					layout(location = 0) out vec4 oColor;
+
+					void main()
+					{
+						oColor = vColor;
+					}
+				)"
+			},
+		}
+	};
+}
+
+hamon::Program GetHLSLProgram(void)
+{
+	return
+	{
+		hamon::ShaderLanguage::HLSL,
+		{
+			{
+				hamon::ShaderStage::Vertex,
+				R"(
+					struct VS_INPUT
+					{
+						float3 pos   : POSITION;
+						float4 color : COLOR;
+					};
+
+					struct VS_OUTPUT
+					{
+						float4 pos   : SV_POSITION;
+						float4 color : COLOR;
+					};
+
+					VS_OUTPUT main(VS_INPUT input)
+					{
+						VS_OUTPUT output;
+						output.pos = float4(input.pos, 1.0);
+						output.color = input.color;
+						return output;
+					}
+				)"
+			},
+			{
+				hamon::ShaderStage::Fragment,
+				R"(
+					struct PS_INPUT
+					{
+						float4 pos   : SV_POSITION;
+						float4 color : COLOR;
+					};
+
+					float4 main(PS_INPUT input) : SV_Target
+					{
+						return input.color;
+					}
+				)"
+			},
+		}
+	};
+}
+
+}
+
+int main()
+{
+	std::uint32_t const width  = 640;
+	std::uint32_t const height = 480;
+
+	std::vector<std::unique_ptr<hamon::Window>>   windows;
+	std::vector<std::unique_ptr<hamon::Renderer>> renderers;
+	std::vector<hamon::Program>					  programs;
+
+#if defined(HAMON_HAS_OPEN_GL)
+	{
+		auto window = std::make_unique<hamon::Window>(width, height, "sample_render_primitive_topology OpenGL");
+		auto renderer = std::make_unique<hamon::GLRenderer>(*window);
+		windows.push_back(std::move(window));
+		renderers.push_back(std::move(renderer));
+		programs.push_back(GetGLSLProgram());
+	}
+#endif
+#if defined(HAMON_HAS_D3D11)
+	{
+		auto window = std::make_unique<hamon::Window>(width, height, "sample_render_primitive_topology D3D11");
+		auto renderer = std::make_unique<hamon::D3D11Renderer>(*window);
+		windows.push_back(std::move(window));
+		renderers.push_back(std::move(renderer));
+		programs.push_back(GetHLSLProgram());
+	}
+#endif
+#if defined(HAMON_HAS_D3D12)
+	{
+		auto window = std::make_unique<hamon::Window>(width, height, "sample_render_primitive_topology D3D12");
+		auto renderer = std::make_unique<hamon::D3D12Renderer>(*window);
+		windows.push_back(std::move(window));
+		renderers.push_back(std::move(renderer));
+		programs.push_back(GetHLSLProgram());
+	}
+#endif
+#if defined(HAMON_HAS_VULKAN)
+	{
+		auto window = std::make_unique<hamon::Window>(width, height, "sample_render_primitive_topology Vulkan");
+		auto renderer = std::make_unique<hamon::VulkanRenderer>(*window);
+		windows.push_back(std::move(window));
+		renderers.push_back(std::move(renderer));
+		programs.push_back(GetGLSLProgram());
+	}
+#endif
+
+	hamon::VertexLayout const vertex_layout
+	{
+		28,
+		{
+			{ hamon::Semantic::Position, hamon::Type::Float, 3, 0 },
+			{ hamon::Semantic::Color,    hamon::Type::Float, 4, 12 },
+		},
+	};
+
+	std::vector<float> const vertices
+	{
+		 0.0f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f, 1.0f,
+		 0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f, 1.0f,
+		-0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f, 1.0f,
+	};
+
+	hamon::Geometry const triangle_geometry
+	{
+		hamon::PrimitiveTopology::TriangleList, vertex_layout, vertices
+	};
+
+	hamon::Geometry const line_geometry
+	{
+		hamon::PrimitiveTopology::LineList, vertex_layout, vertices
+	};
+
+	hamon::Geometry const point_geometry
+	{
+		hamon::PrimitiveTopology::PointList, vertex_layout, vertices
+	};
+
+	int count = 0;
+	for (;;)
+	{
+		for (auto& window : windows)
+		{
+			if (!window->Update())
+			{
+				return 0;
+			}
+		}
+
+		hamon::RenderPassState render_pass;
+		render_pass.clear_value.color.r = 0.4f;
+		render_pass.clear_value.color.g = 0.4f;
+		render_pass.clear_value.color.b = 0.4f;
+		render_pass.clear_value.color.a = 1.0f;
+		render_pass.clear_value.depth = 0.0f;
+		render_pass.clear_value.stencil = 0;
+		render_pass.viewport.width  = width;
+		render_pass.viewport.height = height;
+
+		int i = 0;
+		for (auto& renderer : renderers)
+		{
+			renderer->Begin();
+			renderer->BeginRenderPass(render_pass);
+
+			switch ((count / 60) % 3)
+			{
+			case 0:
+				renderer->Render(triangle_geometry, programs[i], {}, {});
+				break;
+			case 1:
+				renderer->Render(line_geometry, programs[i], {}, {});
+				break;
+			case 2:
+				renderer->Render(point_geometry, programs[i], {}, {});
+				break;
+			}
+
+			renderer->EndRenderPass();
+			renderer->End();
+			++i;
+		}
+
+		++count;
+	}
+
+	return 0;
+}
