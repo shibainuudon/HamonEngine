fc50b493f918ae52a0be0b3c1c3f2f626edb87fc
shibainuudon <shibainuudon@gmail.com>
2022-05-09 23:46:02 +0900

    Uniformをdetailネームスペースに移動 refs #62

---
+++ libs/render/include/hamon/render/detail/uniform.hpp
@@ -0,0 +1,92 @@
+﻿/**
+ *	@file	uniform.hpp
+ *
+ *	@brief	Uniform
+ */
+
+#ifndef HAMON_RENDER_DETAIL_UNIFORM_HPP
+#define HAMON_RENDER_DETAIL_UNIFORM_HPP
+
+#include <algorithm>	// ranges::copy
+#include <concepts>		// copy_constructible
+#include <ranges>		// ranges::data, ranges::range
+#include <memory>		// addressof, shared_ptr
+
+namespace hamon
+{
+
+inline namespace render
+{
+
+namespace detail
+{
+
+class UniformBase
+{
+public:
+	virtual ~UniformBase(){}
+
+	virtual void const* GetData(void) const = 0;
+};
+
+template <typename T>
+class Uniform : public UniformBase
+{
+public:
+	explicit Uniform(T const& value)
+		requires std::copy_constructible<T>
+		: m_value{value}
+	{}
+
+	explicit Uniform(T const& value)
+	{
+		std::ranges::copy(value, std::ranges::data(m_value));
+	}
+
+	void const* GetData(void) const override
+	{
+		return GetDataImpl();
+	}
+
+private:
+	void const* GetDataImpl(void) const
+		requires std::ranges::range<T>
+	{
+		return std::ranges::data(m_value);
+	}
+
+	void const* GetDataImpl(void) const
+	{
+		return std::addressof(m_value);
+	}
+
+private:
+	T	m_value;
+};
+
+class UniformProxy
+{
+public:
+	explicit UniformProxy(std::shared_ptr<UniformBase>& ref)
+		: m_ref(ref)
+	{}
+
+	template <typename T>
+	UniformProxy& operator=(T const& value)
+	{
+		m_ref = std::make_shared<Uniform<T>>(value);
+		return *this;
+	}
+
+private:
+	std::shared_ptr<UniformBase>&	m_ref;
+};
+
+
+}	// namespace detail
+
+}	// inline namespace render
+
+}	// namespace hamon
+
+#endif // HAMON_RENDER_DETAIL_UNIFORM_HPP
--- libs/render/include/hamon/render/uniforms.hpp
+++ libs/render/include/hamon/render/uniforms.hpp
@@ -7,6 +7,7 @@
 #ifndef HAMON_RENDER_UNIFORMS_HPP
 #define HAMON_RENDER_UNIFORMS_HPP
 
+#include <hamon/render/detail/uniform.hpp>
 #include <map>
 #include <memory>
 #include <string>
@@ -17,76 +18,15 @@ namespace hamon
 inline namespace render
 {
 
-class UniformBase
-{
-public:
-	virtual ~UniformBase(){}
-
-	virtual void const* GetData(void) const = 0;
-};
-
-template <typename T>
-class Uniform : public UniformBase
-{
-public:
-	explicit Uniform(T const& value)
-		requires std::copy_constructible<T>
-		: m_value{value}
-	{}
-
-	explicit Uniform(T const& value)
-	{
-		std::ranges::copy(value, std::ranges::data(m_value));
-	}
-
-	void const* GetData(void) const override
-	{
-		return GetDataImpl();
-	}
-
-private:
-	void const* GetDataImpl(void) const
-		requires std::ranges::range<T>
-	{
-		return std::ranges::data(m_value);
-	}
-
-	void const* GetDataImpl(void) const
-	{
-		return std::addressof(m_value);
-	}
-
-private:
-	T	m_value;
-};
-
-class UniformProxy
-{
-public:
-	explicit UniformProxy(std::shared_ptr<UniformBase>& ref)
-		: m_ref(ref)
-	{}
-
-	template <typename T>
-	UniformProxy& operator=(T const& value)
-	{
-		m_ref = std::make_shared<Uniform<T>>(value);
-		return *this;
-	}
-
-private:
-	std::shared_ptr<UniformBase>&	m_ref;
-};
-
 class Uniforms
 {
 public:
-	UniformProxy operator[](std::string const& key)
+	detail::UniformProxy operator[](std::string const& key)
 	{
-		return UniformProxy{m_map[key]};
+		return detail::UniformProxy{m_map[key]};
 	}
 
-	UniformBase const* operator[](std::string const& key) const
+	detail::UniformBase const* operator[](std::string const& key) const
 	{
 		auto it = m_map.find(key);
 		if (it != m_map.end())
@@ -97,7 +37,7 @@ public:
 	}
 
 private:
-	std::map<std::string, std::shared_ptr<UniformBase>>	m_map;
+	std::map<std::string, std::shared_ptr<detail::UniformBase>>	m_map;
 };
 
 }	// inline namespace render
